7b1b75cd951f51801006f87f343e77694e4e0533
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2021-01-09 05:57:26 +0100

    use forms.SplitDateTimeField for date_valid in Transaction Add and Edit fixes #1

--- apps/transactions/forms.py
+++ apps/transactions/forms.py
@@ -2,17 +2,19 @@ from django import forms
 import datetime
 from apps.transactions.models import Transaction
 from apps.rates.models import ExchangeRate
-#from datetimepicker.widgets import DateTimePicker
-#from django.contrib.admin.widgets import AdminDateWidget
 
 class TransactionForm(forms.ModelForm):
+    date_valid = forms.SplitDateTimeField(
+        label="Date and Time of Transaction",
+        input_time_formats=['%H:%M'],
+        widget=forms.SplitDateTimeWidget(
+            date_attrs={'type': 'date'},
+            time_attrs={'type': 'time'},
+            time_format='%H:%M'))
 
     class Meta:
         model = Transaction
-        fields = "__all__"  
-
-        # https://stackoverflow.com/a/52702275/1632368
-        #date_valid = forms.DateField(widget=AdminDateWidget())
+        fields = "__all__"
 
         exchangerates = ExchangeRate.objects.filter(datetime_valid__gt = datetime.datetime.today() - datetime.timedelta(days=7))
         crypto_currencies = [(x, x) for x in sorted({ex.crypto_currency for ex in exchangerates})]
@@ -22,4 +24,3 @@ class TransactionForm(forms.ModelForm):
             'fiat_currency': forms.Select(choices = fiat_currencies),
             'crypto_currency': forms.Select(choices = crypto_currencies),
         }
-
