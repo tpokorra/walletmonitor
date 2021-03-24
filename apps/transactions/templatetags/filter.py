91c264e9cd5649ac4c500b1c1fc0763601203df5
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2021-03-24 21:25:47 +0100

    display small crypto amounts with more decimals fixes #23

--- apps/transactions/templatetags/filter.py
+++ apps/transactions/templatetags/filter.py
@@ -6,5 +6,9 @@ register = template.Library()
 def formatcurrency(value):
     if value > 100:
         return "%0.0f" % (value,)
+    if value < 0.01:
+        return "%0.4f" % (value,)
+    if value < 0.1:
+        return "%0.3f" % (value,)
     return "%0.2f" % (value,)
 
